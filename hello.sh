436fd294bb9a741657e830ac79b8ed7dd670cbd4
Luna Daidouji <luna.daidouji@gmail.com>
2023-07-04 16:16:43 -0300

    Modificado pra teste

--- hello.sh
+++ hello.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 clear;
 echo "Falou mundão!";
+echo "";
+echo "Pressione qualquer tecla para fechar.";
+# -s: Do not echo input coming from a terminal
+# -n 1: Read one character
+read -s -n 1
+echo "";
+echo "Vlw flw!"
 exit;
